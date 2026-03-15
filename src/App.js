ad6441e8a03946699f80408c8f14dddcab5b4c1c
hcbravin <hcbravin@hotmail.com>
2026-03-15 00:27:20 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="container-flex">
+      <header className="text-bg-secondary">
+        <img src={logo} className="" width={200} alt="logo" />
         <a
-          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          className="btn btn-primary"
         >
           Learn React
         </a>
